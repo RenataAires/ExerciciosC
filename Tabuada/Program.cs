cb7721a73fc773e3e807dc94859b0f17e1ce2fa5
RenataAires <renataaires8332@gmail.com>
2026-08-13 19:55:27 -0300

    Exercício 08: Tabuada.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Dê um número para a tabuada");
+int numero = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
