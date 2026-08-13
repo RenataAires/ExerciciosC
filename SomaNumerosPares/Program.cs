38a80e685be297d03c98bd90749d87712c173b57
RenataAires <renataaires8332@gmail.com>
2026-08-13 20:04:34 -0300

    Exercício 09: Soma de números pares.

---
+++ SomaNumerosPares/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("----Vamos somar os números pares de 1 a 100----");
+
+int soma = 0;
+for (int i = 1; i <= 100; i++)
+{
+    if(i % 2 == 0)
+    {
+        soma += i;
+    }
+}
+
+Console.WriteLine($"A soma dos números pares de 1 a 100 é: {soma}");
