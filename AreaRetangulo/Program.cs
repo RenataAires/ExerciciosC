9044afdd19a10d7ea2b2c85fc4797403b14d30c6
RenataAires <renataaires8332@gmail.com>
2026-08-13 15:07:13 -0300

    Exercício 03: Cálculo da área do retângulo concluído.

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Vamos calcular a área de um retângulo!");
+Console.Write("Digite a base: ");
+var baseRetangulo = Console.ReadLine();
+Console.Write("Digite a altura: ");
+var alturaRetangulo = Console.ReadLine();
+
+if(!double.TryParse(baseRetangulo, out double baseDouble))
+{
+    Console.WriteLine($"O valor {baseRetangulo} digitado não é um número válido");
+    Console.ReadKey(true);
+    return;
+}
+
+if(!double.TryParse(alturaRetangulo, out double alturaDouble))
+{
+    Console.WriteLine($"O valor {alturaRetangulo} digitado não é um número válido");
+    Console.ReadKey(true);
+    return;
+}
+
+var area = baseDouble * alturaDouble;
+Console.WriteLine($"A área do retângulo é {area}");
