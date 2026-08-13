26753b5916b7b4b64506cb6e8ac12431eed3567a
RenataAires <renataaires8332@gmail.com>
2026-08-13 15:22:05 -0300

    Exercício 04: Conversor de moeda com arredondamento.

---
+++ ConversorMoedas/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Conversor de moedas");
+Console.Write("Digite o valor em reais: ");
+var valorReais = Console.ReadLine();
+
+if(!double.TryParse(valorReais,  out double valorDouble))
+{
+    Console.WriteLine($"O valor {valorReais} digitado não é um número válido");
+    Console.ReadKey(true);
+    return;
+}
+
+var valorDolar = valorDouble / 5.21;
+Console.WriteLine($"O valor em dólares é {valorDolar:F2}");
+
