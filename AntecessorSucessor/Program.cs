d978ed8065513e755eafbb099027bd59eaf3d317
RenataAires <renataaires8332@gmail.com>
2026-08-13 14:57:18 -0300

    Exercício 02: Refatorado para usar TryParse

---
+++ AntecessorSucessor/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("-----Olá! Me diga um número e eu te direi o antecessor e o sucessor-----");
+var numero = Console.ReadLine();
+
+if(!int.TryParse(numero, out int num))
+{
+    Console.WriteLine($"O valor {numero} digitado não é um número inteiro");
+    Console.ReadKey(true);
+    return;
+}
+var antecessor = num - 1;
+var sucessor = num + 1;
+Console.WriteLine($"O antecessor de {num} é {antecessor} e o sucessor é {sucessor}");
