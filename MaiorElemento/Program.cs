19b9e3a0d9551c8de0517e14200932925b816012
RenataAires <renataaires8332@gmail.com>
2026-08-14 16:58:33 -0300

    Exercício 14: Localizar o maior valor e a posição com loop for.

---
+++ MaiorElemento/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("-----Localizando o maior valor-----");
+
+int maiorValor = int.MinValue;
+int posicao = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Digite o {i}º valor:");
+    if (!int.TryParse(Console.ReadLine(), out int numeroAtual))
+    {
+        Console.WriteLine("Valor inválido. Digite um número inteiro.");
+        i--; // Repetir a leitura para esta posição
+        continue;
+    }
+
+    if (numeroAtual > maiorValor)
+    {
+        maiorValor = numeroAtual;
+        posicao = i;
+    }
+}
+
+Console.WriteLine("\n------------------------------------");
+Console.WriteLine($"O maior valor digitado foi: {maiorValor}");
+Console.WriteLine($"Ele apareceu na {posicao}ª posição.");;
