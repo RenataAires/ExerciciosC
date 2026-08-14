efc0cbcac9c1db81c8a0eda64eb8d1b3f83ba314
RenataAires <renataaires8332@gmail.com>
2026-08-14 18:49:49 -0300

    Exercício 16: Contagem de pares a partir da entrada do usuário.

---
+++ ContagemPares/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Contador de números pares");
+
+int contadorPares = 0;
+for (int i = 1; i <=20 ; i++)
+{
+    Console.Write($"Digite o {i}º número: ");
+
+    string entrada = Console.ReadLine() ?? "";
+
+    if(!int.TryParse(entrada, out int numero))
+    {
+        Console.WriteLine("Valor inválido. Digite um número inteiro.");
+        i--; // Repetir a leitura para esta posição
+        continue;
+    }   
+
+    if( numero % 2 == 0)
+    {
+        contadorPares++;
+    }
+}
+Console.WriteLine($"Você digitou 20 números e {contadorPares} são pares.");
