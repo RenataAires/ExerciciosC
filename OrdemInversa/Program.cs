fe20edafbeafe8bfa10480d9824456ffe78e720f
RenataAires <renataaires8332@gmail.com>
2026-08-14 19:00:24 -0300

    Exercício 17: Inversão de array com 10 elementos.

---
+++ OrdemInversa/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Digite 10 números e eu te devolvo na ordem inversa!");
+
+Console.WriteLine("------------------------------------");
+Console.WriteLine("Digite os números:");
+
+int[] numeros = new int[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (!int.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("Valor inválido. Digite um número inteiro.");
+        i--; // Repetir a leitura para esta posição
+        continue;
+    }
+}
+
+Console.WriteLine("Os números na ordem inversa são: ");
+for (int i = numeros.Length - 1; i >= 0; i--)
+{
+    Console.Write($"{numeros[i]} ");
+}
+Console.WriteLine();
