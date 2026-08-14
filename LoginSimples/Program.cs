3599bddbe65758edb418a3c1f97ed0372ee111a9
RenataAires <renataaires8332@gmail.com>
2026-08-13 21:28:49 -0300

    Exercício 10: Login Simples.

---
+++ LoginSimples/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Bem-vindo ao sistema de login!");
+
+Console.Write("Digite seu nome de usuário: ");
+string usuario = Console.ReadLine() ?? "";
+
+Console.Write("Digite sua senha: ");
+string senha = Console.ReadLine() ?? "";
+
+while (usuario != "admin" || senha != "1234")
+{
+    Console.WriteLine("Usuário ou senha incorretos. Tente novamente.");
+    Console.Write("Digite seu nome de usuário: ");
+    usuario = Console.ReadLine() ?? "";
+
+    Console.Write("Digite sua senha: ");
+    senha = Console.ReadLine() ?? "";
+}
+
+Console.WriteLine("Login bem-sucedido! Bem-vindo ao sistema.");
