20eebefc176ab4c5297580834adbb4e07810f3ea
RenataAires <renataaires8332@gmail.com>
2026-08-13 22:30:41 -0300

    Exercício 12: Contador de caracteres.

---
+++ ContadorCaracteres/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("----Bem-vindo(a) ao Contador de Caracteres----");
+Console.WriteLine("Digite um nome para contar os caracteres:");
+
+string? nome = Console.ReadLine();
+nome ??= string.Empty;
+
+int quantidadeCaracteres = nome.Length;
+Console.WriteLine($"O nome '{nome}' possui {quantidadeCaracteres} caracteres.");
+
