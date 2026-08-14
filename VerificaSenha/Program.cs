6a022392a3fec5cde687c1ff5fe1e41dc7972893
RenataAires <renataaires8332@gmail.com>
2026-08-13 22:57:04 -0300

    Exercício 13: Verificador de senha.

---
+++ VerificaSenha/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("---------Verificador de Senha---------");
+
+Console.WriteLine("Digite uma senha para verificar se ela é forte:");
+string senha = Console.ReadLine() ?? "";
+
+if (senha.Length >= 8)
+{
+    Console.WriteLine("Senha aprovada! Ela possui pelo menos 8 caracteres.");
+}
+else
+{
+    int faltam = 8 - senha.Length;
+    Console.WriteLine("A senha é fraca.");
+    Console.WriteLine($"Ela tem apenas {senha.Length} caracteres. Faltam {faltam} para atingir o mínimo.");
+}
