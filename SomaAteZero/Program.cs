e59ac977bb510e2db22b831b6cc997a30aea625a
RenataAires <renataaires8332@gmail.com>
2026-08-13 22:20:17 -0300

    Exercício 11: renomeado para SomaAteZero e finalizado.

---
+++ SomaAteZero/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Soma de números - Digite zero para encerrar o programa");    
+
+int soma = 0;
+int numero;
+
+do
+{
+    Console.Write("Digite um número: ");
+    string entrada = Console.ReadLine() ?? "";
+
+    if (!int.TryParse(entrada, out numero))
+    {
+        Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+        continue; // Volta para o início do loop para solicitar a entrada novamente
+    }
+    
+        soma += numero;
+
+} while (numero != 0); 
+
+Console.WriteLine($"\nPrograma encerrado.A soma dos números digitados é: {soma}");
