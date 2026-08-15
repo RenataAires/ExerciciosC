83f50bf79dde3ca14380ba499eaf4e2b9a49443f
RenataAires <renataaires8332@gmail.com>
2026-08-14 22:14:21 -0300

    Exercício 18: Número aleatório.

---
+++ JogoSenha/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Adivinhe qual é o número secreto entre 1 e 100!");
+
+int numeroSecreto = Random.Shared.Next(1, 101);
+
+int tentativas = 0;
+
+Console.WriteLine("------------------------------------");
+Console.WriteLine("Digite um número entre 1 e 100:");
+
+while (true)
+{
+    if (!int.TryParse(Console.ReadLine(), out int palpite))
+    {
+        Console.WriteLine("Valor inválido. Digite um número inteiro entre 1 e 100.");
+        continue;
+    }
+
+    tentativas++;
+
+    if (palpite < numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é maior! Tente novamente:");
+    }
+    else if (palpite > numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é menor! Tente novamente:");
+    }
+    else
+    {
+        Console.WriteLine($"Parabéns! Você acertou o número secreto {numeroSecreto} em {tentativas} tentativas.");
+        break;
+    }
+}
